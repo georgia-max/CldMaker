daa35f36b0d48184640f1aef0e6756e40533cff4
georgia-max <georgialiu63@gmail.com>
2023-09-12 15:55:06 -0400

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,46 @@
+"""Python setup.py for project_name package"""
+import io
+import os
+from setuptools import find_packages, setup
+
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("CldMaker", "0.01")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
+
+setup(
+    name="CldMaker",
+    version=read("CldMaker", "0.01"),
+    description="project_description",
+    url="https://github.com/georgia-max/CldMaker/",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="author_name",
+    packages=find_packages(exclude=["tests", ".github"]),
+    install_requires=read_requirements("requirements.txt"),
+    entry_points={
+        "console_scripts": ["project_name = project_name.__main__:main"]
+    },
+    extras_require={"test": read_requirements("requirements-test.txt")},
+)
