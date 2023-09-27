a9fcc813b47a50637087d884470261470aeeb57a
georgia-max <georgialiu63@gmail.com>
2023-09-27 15:08:36 -0400

    add user inputv2

--- CldMaker/main.py
+++ CldMaker/main.py
@@ -2,9 +2,6 @@ from flask import Flask
 from flask import request
 import cld_maker
 
-# import pandas as pd
-# import glob
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,7 +15,7 @@ def home():
         result = ""
     return (
         """<form action="" method="get">
-        
+
             Hypothesis: <input type="text" name="my_hypothesis" style="height: 300px ">
             Variables: <input type="text" name="my_variables" style="height: 300px ">
             <input type="submit" value="Convert" >
@@ -28,33 +25,8 @@ def home():
         + result
     )
 
-
-    # result = cld_maker.cld_maker()
-    # return result
-
-
-# def index():
-#     print(request.args)
-#     celsius = request.args.get("celsius", "")
-#     if celsius:
-#         fahrenheit = fahrenheit_from(celsius)
-#     else:
-#         fahrenheit = ""
-#     return (
-#         """<form action="" method="get">
-#             Celsius Temperature: <input type="text" name="celsius">
-#             <input type="submit" value="Convert">
-#         </form>"""
-#         + "Fahrenheit: "
-#         + fahrenheit
-#     )
-
-
-
-
 if __name__ == '__main__':
     app.run(host = "127.0.0.1", port = 8080, debug=True)
-    # prompts_df = read_dataset('CldMaker/prompt_dict.json')
 
     # result = cld_maker.cld_maker()
     # print(result)
