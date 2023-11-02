ed5d14c66df2178a2bd4b9f7b00155dfee8848e5
Yi-Han Chen <hannah51125@gmail.com>
2023-11-01 22:56:39 -0400

    func: Create opinion in db

---
+++ test_create_db.py
@@ -0,0 +1,17 @@
+from repository.opinion import OpinionRepository
+
+
+class TestCreateOpinion:
+
+    def setup_method(self):
+        self.repo = OpinionRepository()
+
+    def teardown_method(self):
+        self.repo.truncate_table()
+
+    def test_create_opinion(self):
+        opinion_id = self.repo.create_opinion(
+            "Test User Input Opinion", "Test Predicted Var", "Test Predicted Graph")
+
+        res = self.repo.read_option(opinion_id)
+        assert res is not None
