c03d5e3ed196e1d9d188431a3c9a85640dc38bb5
Yi-Han Chen <hannah51125@gmail.com>
2023-12-18 12:29:04 +0800

    infra: Copy main.py as app.py for azure deployment

---
+++ app.py
@@ -0,0 +1,52 @@
+import graphviz
+from dotenv import load_dotenv
+from flask import Flask
+from flask import request
+
+from CldMaker.cld_maker import GraphGenerator
+from CldMaker.service.openai_service import OpenAIService
+from repository.opinion import OpinionRepository
+
+app = Flask(__name__)
+
+load_dotenv(override=True)
+
+
+@app.route('/')
+def home():
+    print(request.args)
+    my_hypothesis = request.args.get("my_hypothesis", "")
+    svg_str = ""
+    if my_hypothesis:
+        generator = GraphGenerator(OpenAIService())
+        graph_result = generator.generate_by_hypothesis(my_hypothesis)
+    else:
+        graph_result = ""
+    if graph_result != "":
+        repo = OpinionRepository()
+        repo.create_opinion(my_hypothesis, "", graph_result)
+        # Convert graphviz src to svg
+        g_src = graphviz.Source(graph_result)
+        svg_str = g_src.pipe(format="svg", encoding='utf-8')
+    return (
+            """<form action="" method="get">
+                <label for="my_hypothesis">Please enter your dynamic hypothesis in the text box below:</label>
+                <br>
+    
+                Hypothesis: <input type="text" name="my_hypothesis" style="height: 200px; width: 700px; ">
+                <br>
+                <input type="submit" value="Convert" style="margin-left: 10px;">
+                
+            </form>"""
+            + "Result: "
+            + graph_result +
+            f"""
+            <div class="graphviz-output">
+            {svg_str}
+            </div>
+            """
+    )
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
