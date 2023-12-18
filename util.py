916f8a5bf275940114626ff56f1d78afac509ff9
Yi-Han Chen <hannah51125@gmail.com>
2023-12-18 11:27:24 +0800

    infra/k8s-db: DB with user and password

--- util.py
+++ util.py
@@ -7,16 +7,21 @@ def get_connection():
     db_host = os.getenv('DB_HOST')
     db_port = os.getenv('DB_PORT')
     db_name = os.getenv('DB_NAME')
-    # TODO, uncomment when you have a database user and password
     db_user = os.getenv('DB_USER')
     db_password = os.getenv('DB_PASSWORD')
 
-    conn = psycopg2.connect(
-        host=db_host,
-        port=db_port,
-        database=db_name,
-        # user=db_user,
-        # password=db_password
-    )
-
+    if db_user and db_password:
+        conn = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            database=db_name,
+            user=db_user,
+            password=db_password
+        )
+    else:
+        conn = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            database=db_name,
+        )
     return conn
